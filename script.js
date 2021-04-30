1437a12ae907bff47c1aa74cb0625e191ef88fab
Pablo <pgocev@mail.ru>
2021-04-30 21:55:08 +0300

    cycles and conditions

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const numberOfFilm = prompt('Сколько фильмов вы уже посмотрели?', '');
+if(numberOfFilm<10) {
+    alert('Просмотрено довольно мало фильмов');
+}
+else if (numberOfFilm>10 && numberOfFilm<30) {
+    alert('Вы классический зритель');
+}
+else if (numberOfFilm>30) {
+    alert('Вы киноман');
+}
+else {
+    alert('Произошла ошибка');
+}
 const personalMovieDB = {
     count: numberOfFilm,
     movies: {},
@@ -6,9 +18,36 @@ const personalMovieDB = {
     genres: [],
     privat: false
 }
-const a = prompt('Один из последних просмотренных фильмов',''),
-      b = prompt('На сколько вы оцените его?');
-personalMovieDB.movies[a] = b;
+for(let i = 0; i< personalMovieDB.count; i++)
+{
+    let a = prompt(`Введите название ${i+1} фильма`,'');
+    while (a.length===0 || a.length>50){
+        a = prompt(`Введите название ${i+1} фильма`,'');
+    }
+    const b = prompt('На сколько вы оцените его?');
+    personalMovieDB.movies[a] = b;
+}
+// let i = 0;
+// while(i<personalMovieDB.count)
+// {
+//     let a = prompt(`Введите название ${i+1} фильма`,'');
+//     while (a.length===0 || a.length>50){
+//         a = prompt(`Введите название ${i+1} фильма`,'');
+//     }
+//     const b = prompt('На сколько вы оцените его?');
+//     personalMovieDB.movies[a] = b;
+//     i++;
+// }
+// do {
+//     let a = prompt(`Введите название ${i+1} фильма`,'');
+//     while (a.length===0 || a.length>50){
+//         a = prompt(`Введите название ${i+1} фильма`,'');
+//     }
+//     const b = prompt('На сколько вы оцените его?');
+//     personalMovieDB.movies[a] = b;
+//     i++;
+// } while(i<personalMovieDB.count);
+
 console.log(personalMovieDB);
 
 
