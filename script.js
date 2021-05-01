1efabcb3e4315c4d6bb977d48adb742f6e3ea65b
Pablo <pgocev@mail.ru>
2021-05-01 14:02:48 +0300

    Lesson 2: task 1 (added functions)

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-const numberOfFilm = prompt('Сколько фильмов вы уже посмотрели?', '');
-if(numberOfFilm<10) {
-    console.log('Просмотрено довольно мало фильмов');
+'use strict';
+
+let numberOfFilm = prompt('Сколько фильмов вы уже посмотрели?', '');
+function start() {
+    numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while(numberOfFilm == '' || numberOfFilm == null || isNaN(numberOfFilm)) {
+        numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
 }
-else if (numberOfFilm>10 && numberOfFilm<30) {
-    console.log('Вы классический зритель');
-}
-else if (numberOfFilm>30) {
-    console.log('Вы киноман');
-}
-else {
-    console.log('Произошла ошибка');
-}
+start();
+
 const personalMovieDB = {
     count: numberOfFilm,
     movies: {},
@@ -18,37 +16,36 @@ const personalMovieDB = {
     genres: [],
     privat: false
 }
-for(let i = 0; i< personalMovieDB.count; i++)
-{
-    let a = prompt(`Введите название ${i+1} фильма`,'');
-    while (a.length===0 || a.length>50 || a===null){
-        console.log('error in film name lenght');
-        a = prompt(`Введите название ${i+1} фильма`,'');
-    }
-    const b = prompt('На сколько вы оцените его?');
-    personalMovieDB.movies[a] = b;
-}
-// let i = 0;
-// while(i<personalMovieDB.count)
-// {
-//     let a = prompt(`Введите название ${i+1} фильма`,'');
-//     while (a.length===0 || a.length>50){
-//         a = prompt(`Введите название ${i+1} фильма`,'');
-//     }
-//     const b = prompt('На сколько вы оцените его?');
-//     personalMovieDB.movies[a] = b;
-//     i++;
-// }
-// do {
-//     let a = prompt(`Введите название ${i+1} фильма`,'');
-//     while (a.length===0 || a.length>50){
-//         a = prompt(`Введите название ${i+1} фильма`,'');
-//     }
-//     const b = prompt('На сколько вы оцените его?');
-//     personalMovieDB.movies[a] = b;
-//     i++;
-// } while(i<personalMovieDB.count);
 
+function rememberMyFilms() {
+    for(let i = 0; i< personalMovieDB.count; i++)
+    {
+        let a = prompt(`Введите название ${i+1} фильма`,'');
+        while (a.length===0 || a.length>50 || a===null){
+            console.log('error in film name lenght');
+            a = prompt(`Введите название ${i+1} фильма`,'');
+        }
+        const b = prompt('На сколько вы оцените его?');
+        personalMovieDB.movies[a] = b;
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if(numberOfFilm<10) {
+        console.log('Просмотрено довольно мало фильмов');
+    }
+    else if (numberOfFilm>10 && numberOfFilm<30) {
+        console.log('Вы классический зритель');
+    }
+    else if (numberOfFilm>30) {
+        console.log('Вы киноман');
+    }
+    else {
+        console.log('Произошла ошибка');
+    }
+}
+detectPersonalLevel();
 console.log(personalMovieDB);
 
 
